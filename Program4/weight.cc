29fa19db1201f7f75f673617cbcf70e4410f5c33
Mikul958 <michaelpikula958@gmail.com>
2023-11-01 22:46:20 -0400

    Updated style according to cpplint

--- Program4/weight.cc
+++ Program4/weight.cc
@@ -2,80 +2,68 @@
 
 #include "weight.h"
 
-ostream& operator << (ostream& whereto, const Weight& w)
-{
+// Overload stream insertion operator
+ostream& operator << (ostream& whereto, const Weight& w) {
     whereto << w.value_ << " " << w.units_;
     return whereto;
 }
 
 // Constructor
-Weight::Weight(double value, string units)
-{
+Weight::Weight(double value, string units) {
     SetValue(value);
     SetUnits(units);
 }
 
 // Accessors
-double Weight::GetValue() const
-{
+double Weight::GetValue() const {
     return value_;
 }
-string Weight::GetUnits() const
-{
+string Weight::GetUnits() const {
     return units_;
 }
 
 // Mutators
-void Weight::SetValue(double value)
-{
+void Weight::SetValue(double value) {
     if (value >= 0)
         value_ = value;
     else
         value_ = 0;
 }
-void Weight::SetUnits(string units)
-{
-    if (units == "ounces" || units == "pounds" || units == "grams" || units == "kilograms")
+void Weight::SetUnits(string units) {
+    if (units == "ounces" || units == "pounds" ||
+            units == "grams" || units == "kilograms")
         units_ = units;
     else
         units_ = "pounds";
 }
 
 // Misc functions
-void Weight::ConvertUnits(string to_units)
-{
+void Weight::ConvertUnits(string to_units) {
     // 1 lb = 453.59237 g
     const double kPoundsToGrams = 453.59237;
 
-    if (units_ == "ounces")
-    {
+    if (units_ == "ounces") {
         if (to_units == "pounds")
             value_ = value_ / 16;
         else if (to_units == "grams")
             value_ = (value_ / 16) * kPoundsToGrams;
         else if (to_units == "kilograms")
             value_ = (value_ / 16) * kPoundsToGrams / 1000;
-    }
-    else if (units_ == "pounds")
-    {
+    } else if (units_ == "pounds") {
         if (to_units == "ounces")
             value_ = value_ * 16;
         else if (to_units == "grams")
             value_ = value_ * kPoundsToGrams;
         else if (to_units == "kilograms")
             value_ = value_ * kPoundsToGrams / 1000;
-    }
-    else if (units_ == "grams")
-    {
+    } else if (units_ == "grams") {
         if (to_units == "ounces")
             value_ = value_ / kPoundsToGrams * 16;
         else if (to_units == "pounds")
             value_ = value_ / kPoundsToGrams;
         else if (to_units == "kilograms")
             value_ = value_ / 1000;
-    }
-    else if (units_ == "kilograms")
-    {
+    } else if (units_ == "kilograms") {
         if (to_units == "ounces")
             value_ = (value_ * 1000) / kPoundsToGrams * 16;
         else if (to_units == "pounds")
@@ -85,12 +73,12 @@ void Weight::ConvertUnits(string to_units)
     }
 
     // Checking unit separately so code blocks above aren't huge.
-    if (to_units == "ounces" || to_units == "pounds" || to_units == "grams" || to_units == "kilograms")
+    if (to_units == "ounces" || to_units == "pounds" ||
+            to_units == "grams" || to_units == "kilograms")
         units_ = to_units;
 }
 
-bool Weight::operator > (const Weight &right) const
-{
+bool Weight::operator > (const Weight &right) const {
     if (units_ == right.units_)
         return value_ > right.value_;
 
@@ -98,8 +86,7 @@ bool Weight::operator > (const Weight &right) const
     right_copy.ConvertUnits(units_);
     return value_ > right_copy.value_;
 }
-bool Weight::operator < (const Weight &right) const
-{
+bool Weight::operator < (const Weight &right) const {
     if (units_ == right.units_)
         return value_ < right.value_;
 
--- Program4/weightrange.cc
+++ Program4/weightrange.cc
@@ -1,58 +1,49 @@
 // Copyright 2023 mpikula
 
 #include "weightrange.h"
+#include <string>
+using std::string;
 
-WeightRange::WeightRange()
-{
+WeightRange::WeightRange() {
     smallest_ = Weight(0, "pounds");
     largest_ = Weight(0, "pounds");
 }
-WeightRange::WeightRange(const Weight smallest, const Weight largest)
-{
-    if (smallest > largest)
-    {
+WeightRange::WeightRange(const Weight smallest, const Weight largest) {
+    if (smallest > largest) {
         smallest_ = largest;
         largest_ = smallest;
-    }
-    else
-    {
+    } else {
         smallest_ = smallest;
         largest_ = largest;
     }
 }
 
-Weight WeightRange::GetSmallest()
-{
+Weight WeightRange::GetSmallest() {
     return smallest_;
 }
-Weight WeightRange::GetLargest()
-{
+Weight WeightRange::GetLargest() {
     return largest_;
 }
 
-void WeightRange::SetSmallest(Weight smallest)
-{
+void WeightRange::SetSmallest(Weight smallest) {
     if (smallest > largest_)
         return;
     smallest_ = smallest;
 }
-void WeightRange::SetLargest(Weight largest)
-{
+void WeightRange::SetLargest(Weight largest) {
     if (largest < smallest_)
         return;
     largest_ = largest;
 }
 
-bool WeightRange::InRange(Weight w, bool inclusive)
-{
+bool WeightRange::InRange(Weight w, bool inclusive) {
     if (!inclusive)
         return w > smallest_ && w < largest_;
 
     // w >= smallest_ && w <= largest_
     return !(w < smallest_) && !(w > largest_);
 }
-Weight WeightRange::Width()
-{
+Weight WeightRange::Width() {
     // Convert a copy of smallest to units of largest.
     Weight smallest_copy(smallest_);
     string largest_units = largest_.GetUnits();
