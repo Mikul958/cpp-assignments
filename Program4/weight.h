7eb25f46bb99cf7714177d4cc402a7f8545af37d
Mikul958 <michaelpikula958@gmail.com>
2023-11-01 22:35:15 -0400

    Fixed style according to cpplint

--- Program4/weight.h
+++ Program4/weight.h
@@ -8,27 +8,26 @@ using std::string;
 #include <iostream>
 using std::ostream;
 
-class Weight
-{
+class Weight {
     friend ostream& operator << (ostream& whereto, const Weight& w);
 
-    public:
-        explicit Weight(double value = 0, string units = "pounds");
+ public:
+    explicit Weight(double value = 0, string units = "pounds");
 
-        double GetValue() const;
-        string GetUnits() const;
+    double GetValue() const;
+    string GetUnits() const;
 
-        void SetValue(double value);
-        void SetUnits(string units);
+    void SetValue(double value);
+    void SetUnits(string units);
 
-        void ConvertUnits(string to_units);
+    void ConvertUnits(string to_units);
 
-        bool operator > (const Weight &right) const;
-        bool operator < (const Weight &right) const;
+    bool operator > (const Weight &right) const;
+    bool operator < (const Weight &right) const;
 
-    private:
-        double value_;
-        string units_;
+ private:
+    double value_;
+    string units_;
 };
 
 #endif  // _WEIGHT_H_
