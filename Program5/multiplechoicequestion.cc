6b5042e2392d2545cbefa568f8adb2cc00c70367
Mikul958 <michaelpikula958@gmail.com>
2023-11-29 17:00:01 -0500

    Fixed segfault and fixed style according to cpplint

--- Program5/multiplechoicequestion.cc
+++ Program5/multiplechoicequestion.cc
@@ -6,67 +6,63 @@
 using std::cout;
 using std::endl;
 
-namespace csce240_program5
-{
+namespace csce240_program5 {
 
-MultipleChoiceQuestion::MultipleChoiceQuestion(string q, int num, string * c, bool * ans) : Question(q)
-{
+MultipleChoiceQuestion::MultipleChoiceQuestion(string q, int num, string * c,
+                                               bool * ans) : Question(q) {
     num_choices_ = 0;
-    choices_ = new string[0];
-    answers_ = new bool[0];
+    choices_ = new string[1];
+    answers_ = new bool[1];
+    choices_[0] = "";
+    answers_[0] = true;
     SetAnswerChoices(num, c, ans);
 }
 
-MultipleChoiceQuestion::MultipleChoiceQuestion(const MultipleChoiceQuestion& toCopy) : Question(toCopy.GetQuestion())
-{
+MultipleChoiceQuestion::MultipleChoiceQuestion
+                        (const MultipleChoiceQuestion& toCopy)
+                        : Question(toCopy.GetQuestion()) {
     num_choices_ = toCopy.num_choices_;
     choices_ = new string[num_choices_];
     answers_ = new bool[num_choices_];
 
-    for (int i=0; i<num_choices_; i++)
-    {
+    for (int i=0; i < num_choices_; i++) {
         choices_[i] = toCopy.choices_[i];
         answers_[i] = toCopy.answers_[i];
     }
 }
-MultipleChoiceQuestion& MultipleChoiceQuestion::operator = (const MultipleChoiceQuestion& right)
-{
+MultipleChoiceQuestion& MultipleChoiceQuestion::operator =
+                        (const MultipleChoiceQuestion& right) {
     SetQuestion(right.GetQuestion());
     num_choices_ = right.num_choices_;
     delete [] choices_;
     delete [] answers_;
     choices_ = new string[num_choices_];
     answers_ = new bool[num_choices_];
-    for (int i=0; i<num_choices_; i++)
-    {
+    for (int i=0; i < num_choices_; i++) {
         choices_[i] = right.choices_[i];
         answers_[i] = right.answers_[i];
     }
 
     return *this;
 }
-MultipleChoiceQuestion::~MultipleChoiceQuestion()
-{
+MultipleChoiceQuestion::~MultipleChoiceQuestion() {
     delete [] choices_;
     delete [] answers_;
 }
 
-int MultipleChoiceQuestion::GetNumChoices() const
-{
+int MultipleChoiceQuestion::GetNumChoices() const {
     return num_choices_;
 }
-void MultipleChoiceQuestion::SetAnswerChoices(int num, string * c, bool * ans)
-{
+void MultipleChoiceQuestion::SetAnswerChoices(int num, string * c, bool * ans) {
     if (num < 1)
         return;
     num_choices_ = num;
     delete [] choices_;
     delete [] answers_;
     choices_ = new string[num_choices_];
-    choices_ = new string[num_choices_];
+    answers_ = new bool[num_choices_];
 
-    for (int i=0; i<num_choices_; i++)
-    {
+    for (int i=0; i < num_choices_; i++) {
         // If answer choices parameter is a null pointer, default choices to ""
         if (c == nullptr)
             choices_[i] = "";
@@ -81,12 +77,10 @@ void MultipleChoiceQuestion::SetAnswerChoices(int num, string * c, bool * ans)
     }
 }
 
-void MultipleChoiceQuestion::Print(bool show_answers) const
-{
+void MultipleChoiceQuestion::Print(bool show_answers) const {
     cout << "Question: " << GetQuestion() << endl;
     cout << "Answer Choices:" << endl;
-    for (int i=0; i<num_choices_; i++)
-    {
+    for (int i=0; i < num_choices_; i++) {
         cout << choices_[i];
         if (show_answers)
             cout << (answers_[i] ? " - correct" : " - incorrect");
