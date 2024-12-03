1491cfb3480142bef720350f65003960c002d6c3
Mikul958 <michaelpikula958@gmail.com>
2024-12-03 16:07:26 -0500

    Created base of TuringMachine class

--- CSCE355/TMSimulator/turing_machine.cc
+++ CSCE355/TMSimulator/turing_machine.cc
@@ -1,3 +1,8 @@
 // Copyright 2024 Michael Pikula
 
 #include "turing_machine.h"
+
+// Initialize all states of Turing machine using given text file
+TuringMachine::TuringMachine(string filename) {
+
+}
--- CSCE355/TMSimulator/turing_machine.h
+++ CSCE355/TMSimulator/turing_machine.h
@@ -3,6 +3,37 @@
 #ifndef _TURINGMACHINE_H_
 #define _TURINGMACHINE_H_
 
+#include <string>
+#include <vector>
+#include <unordered_map>
 
+using std::string;
+using std::vector;
+using std::unordered_map;
+
+#include "Tape.h"
+
+// Transition associated with a specific state and input
+struct Transition {
+    char write;       // Symbol to write before head moves
+    bool headRight;   // Direction for head to move; false for left, true for right
+    int next;         // Index of the State this transition will go to.
+};
+
+// State in a Turing machine, containing a name and set of transitions
+struct State {
+    string name;                                  // Name of state as given in text file
+    unordered_map<char, Transition> transitions;  // Hash table mapping a character input to its transition
+};
+
+class TuringMachine
+{
+    public:
+        explicit TuringMachine(string);
+
+    private:
+        vector<State> states;
+        Tape tape;
+};
 
 #endif  // _TURINGMACHINE_H_
